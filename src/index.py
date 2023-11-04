c2111df505ef6aa3e556bd0bfe2a3dcb1e587d1b
Kalle Hahl <kalle.hahl@hotmail.com>
2023-11-04 12:49:15 +0200

    tehtävä 4 tehty, actionit toimii

--- src/index.py
+++ src/index.py
@@ -1,19 +1,23 @@
+
 from varasto import Varasto
 
-
-def main():
+def create_varasto():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
+    return mehua, olutta
 
+def print_initial(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def print_olut_getters(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def mehu_setters(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,36 +26,53 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
-    print("Virhetilanteita:")
+def handle_minus_varasto():
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
     print(huono)
 
+def handle_minus_saldo():
     print("Varasto(100.0, -50.7)")
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def handle_olutta_liikaa(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def handle_mehua_liian_vahan(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
+
+def handle_olutta_liian_vahan(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def handle_mehua_liikaa(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+def main():
+    mehua, olutta = create_varasto()
+    print_initial(mehua, olutta)
+    print_olut_getters(olutta)
+    mehu_setters(mehua)
+    handle_minus_varasto()
+    handle_minus_saldo()
+    handle_olutta_liikaa(olutta)
+    handle_mehua_liian_vahan(mehua)
+    handle_olutta_liian_vahan(olutta)
+    handle_mehua_liikaa(mehua)
 
 if __name__ == "__main__":
     main()
